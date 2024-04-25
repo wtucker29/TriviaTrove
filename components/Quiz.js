c890a2aa54aa10760572f1e23b7537857f7720a2
William Tucker <william.tucker29@gmail.com>
2024-04-25 16:36:44 -0500

    Updated component to show the current question in a white box with the progress bar and question count components included. Currently working on displaying the answer options

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,51 +1,71 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView, Animated } from 'react-native';
+import shuffle from 'lodash/shuffle';
 import QuizQuestion from './QuizQuestion';
+import ProgressBar from './ProgressBar';
+import QuestionCount from './QuestionCount';
 
-export default function Quiz({ route, onAnswerSubmit }) {
+export default function Quiz({ route, navigation }) {
   const { data } = route.params;
   const [selectedAnswers, setSelectedAnswers] = useState(Array(data.length).fill(''));
   const [shuffledOptions, setShuffledOptions] = useState([]);
+  const [progress, setProgress] = useState(new Animated.Value(1));
+  const [fadeAnim, setFadeAnim] = useState(new Animated.Value(1));
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [isOptionsDisabled, setIsOptionsDisabled] = useState(false);
+  const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
+  const [correctOption, setCorrectOption] = useState(null);
+  const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    const options = shuffle([...quiz.incorrect_answers, quiz.correct_answer]);
-    setShuffledOptions(options);
-  }, [quiz]);
 
-  const handleAnswerSelect = (index, answer) => {
-    setSelectedAnswers(prevAnswers => {
-      const updatedAnswers = [...prevAnswers];
-      updatedAnswers[index] = answer;
-      return updatedAnswers;
-    });
-  };
-
-  const handleAnswerSubmit = () => {
-    onAnswerSubmit(selectedAnswers);
-  };
+  const shuffledQuestions = data.map((question) => {
+    const shuffledOptions = shuffle([...question.incorrect_answers, question.correct_answer]);
+    return {
+      ...question,
+      shuffledOptions: shuffledOptions,
+    };
+  });
+  console.log('shuffledoptions: ', shuffledQuestions);
+  // const options = shuffle([...data.incorrect_answers, data.correct_answer]);
+  // console.log(options);
+  // setShuffledOptions(options);
 
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      {/* {quizData.map((quiz, index) => {
-        return (
-          <QuizQuestion
-            index={index}
-            key={quiz.question}
-            quiz={quiz}
-            selectedAnswer={selectedAnswers[index]}
-            onSelect={(answer) => handleAnswerSelect(index, answer)}
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <View style={styles.subContainer}>
+          <ProgressBar progress={progress} />
+          <QuestionCount
+            index={currentQuestionIndex}
+            question={data[currentQuestionIndex]?.question}
           />
-        )
-      })} */}
-    </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#272724',
+  },
   container: {
+    flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    position: 'relative',
+  },
+  subContainer: {
+    marginTop: 50,
+    marginVertical: 10,
+    padding: 40,
+    borderTopRightRadius: 40,
+    borderRadius: 10,
+    backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
-    margin: '1.5em auto',
+    shadowColor: '#171717',
+    shadowOffset: { width: -6, height: 6 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
