5f8d74cf62300471f212661bd4dd1245032ea27c
William Tucker <william.tucker29@gmail.com>
2024-04-25 15:08:56 -0500

    Updated existing Profile page to new page layout. Updated button press to return to home screen

--- components/Profile.js
+++ components/Profile.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Text, Animated } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import ProfileTable from './ProfileTable';
 
-export default function Profile({ onProfileClose }) {
+export default function Profile({ navigation }) {
+  const [fadeAnim, setFadeAnim] = useState(new Animated.Value(1));
+
+  const home = () => {
+    Animated.sequence([
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: false,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1900,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
   return (
     <View style={styles.container} >
-      <View style={styles.section}>
-        <Text style={styles.heading}>Overall Quiz Average:</Text>
-        <Text style={styles.overall}>Total Score: 4</Text>
-        <Text style={styles.overall}>Total Questions: 5</Text>
-        <Text style={styles.overall}>Average Score: 75%</Text>
+      <View style={styles.subContainer}>
+        <Text style={styles.text}>Overall Quiz Average:</Text>
+        <Text style={styles.text}>Total Score: 4</Text>
+        <Text style={styles.text}>Total Questions: 5</Text>
+        <Text style={styles.text}>Average Score: 75%</Text>
       </View>
       {/* <View style={styles.section}>
         <Text style={styles.heading}>Quiz History:</Text>
         <ProfileTable />
       </View> */}
-      <View style={styles.section}>
-        <TouchableOpacity title="Profile" onPress={onProfileClose} accessabilityLabel="Click to view user profile" style={styles.button}>
-          <Text style={styles.buttonText}>Return to Home</Text>
+      <View style={styles.subContainer}>
+        <TouchableOpacity
+          title="Return to Home"
+          onPress={() => {
+            navigation.navigate('Home');
+            home();
+          }}
+          accessabilityLabel="Click to return to home page"
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>Return to Home</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -27,33 +52,41 @@ export default function Profile({ onProfileClose }) {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '1.5em auto',
+    flex: 1,
+    backgroundColor: "#272724",
+    alignItem: "center",
+    justifyContent: "center",
   },
-  section: {
-    marginBottom: 20,
+  image: {
+    width: "100%",
+    height: 350,
+    resizeMode: "contain",
   },
-  heading: {
+  subContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  btn: {
+    backgroundColor: "#efd356",
+    paddingHorizontal: 5,
+    paddingVertical: 15,
+    position: "relative",
+    borderRadius: 15,
+    marginHorizontal: 20,
+    alignItems: "center",
+  },
+  btnText: {
     fontSize: 20,
-    color: '#ffffff',
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  overall: {
-    fontSize: 16,
-    color: '#ffffff',
-    marginBottom: 5,
-  },
-  button: {
-    backgroundColor: '#efd356',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginBottom: 20,
-  },
-  buttonText: {
-    color: 'black',
-    fontSize: 20,
+    textAlign: "center",
+    color: "#000000",
+    letterSpacing: 1.1,
   },
 });
